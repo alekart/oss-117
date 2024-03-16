5a11c0e6e84dc45bf86ad24a92cc05cf79573f88
alek <alekart@users.noreply.github.com>
2024-03-16 16:32:56 +0100

    Implement keyword search in quotes not supported in API

--- src/api.js
+++ src/api.js
@@ -6,40 +6,114 @@ const path = require('path');
  * @property {number} [number]
  * @property {string} [keyword]
  */
+/**
+ * @typedef {Object} Character
+ * @property {string} name
+ * @property {string} slug
+ */
+
+/**
+ * @typedef {Object} CharacterQuotes
+ * @property {string} name
+ * @property {string} slug
+ * @property {string[]} quotes
+ */
+/**
+ * @typedef {Object} Quote
+ * @property {string} sentence
+ * @property {Character} character
+ */
 
 class OSSApi {
   static url = 'https://api.oss117quotes.xyz/v1/';
   static endpoints = {
     random: `random/{number}`,
-    character: `author/{character}/{number}`,
+    author: `author/{character}/{number}`,
+    character: `character/{character}`,
     all: 'characters',
   };
 
   /**
    * Get OSS 117 quotes from the API based on the provided options.
    * @param {Options} options
-   * @returns {string}
+   * @returns {Promise<Quote[]>}
    */
   getQuotes(options) {
     if (options?.keyword) {
       return this.getKeywordQuote(options);
     }
+    let url;
     if (options?.character) {
-      return this.request(this.makeUrl('character', options));
+      url = this.makeUrl('author', options);
+    } else {
+      url = this.makeUrl('random', options);
     }
-    return this.request(this.makeUrl('random', options));
+    return this.request(url).then((data) => !Array.isArray(data) ? [data] : data);
+  }
+
+  /**
+   * @returns {Promise<CharacterQuotes[]>}
+   */
+  getAllQuotes() {
+    const url = this.makeUrl('all');
+    return this.request(url);
+  }
+
+  /**
+   * @param {string} character
+   * @returns {Promise<CharacterQuotes | undefined>}
+   * @private
+   */
+  getAllCharacterQuotes(character) {
+    const url = this.makeUrl('character', {character});
+    return this.request(url).then((data) => data[0]);
   }
 
   /**
    * @param {Options} options
-   * @returns {string}
+   * @returns {Promise<Quote[]>}
    */
   getKeywordQuote(options) {
-    const url = makeUrl('all');
-    // TODO: fetch quotes from the API
-    //  search in all quotes for the keyword
-    //  return the quotes containing the keyword (number or less)
-    //  return an error message if no quotes are found
+    const errorMsg = 'No quotes found for the provided keyword.';
+    if (options.character) {
+      let character;
+      return this.getAllCharacterQuotes(options.character)
+        .then((characterQuotes) => {
+          if (!characterQuotes) {
+            console.log('Wrong character name. Please check the character list.');
+            return [];
+          }
+          return this.findKeywordSentences(characterQuotes, options.keyword);
+        });
+    }
+
+    return this.getAllQuotes().then(
+      (characters) => {
+        const quotes = characters.map((character) => this.findKeywordSentences(character, options.keyword));
+        return quotes.flat().slice(0, options.number || 1);
+      }
+    )
+  }
+
+  /**
+   * Search provided keyword in provided the character quotes.
+   *
+   * @param {CharacterQuotes} characterQuotes
+   * @param {string} keyword
+   * @returns {Quote[]}
+   * @private
+   */
+  findKeywordSentences(characterQuotes, keyword) {
+    const found = characterQuotes.quotes.filter((quote) => {
+      return quote.includes(keyword);
+    });
+    return found.map((quote) => ({
+      sentence: quote,
+      character: {
+        name: characterQuotes.name,
+        slug: characterQuotes.slug,
+      },
+    }));
   }
 
   /**
@@ -57,6 +131,7 @@ class OSSApi {
    * @param {string} endpoint
    * @param {Options} [options]
    * @returns {string}
+   * @private
    */
   makeUrl(endpoint, options) {
     const point = OSSApi.endpoints[endpoint]
