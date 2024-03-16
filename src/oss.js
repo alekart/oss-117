2304b8f38dd86a33230eb738f4838339df38cfd4
alek <alekart@users.noreply.github.com>
2024-03-16 16:33:23 +0100

    Display cli results in terminal

--- src/oss.js
+++ src/oss.js
@@ -1,1 +1,25 @@
-const argument = require('./prompt');
+const args = require('./prompt');
+const OSSApi = require('./api');
+const ossApi = new OSSApi();
+
+/**
+ * @type {Options}
+ */
+const options = {
+  character: args.arguments.character,
+  number: args.arguments.number,
+  keyword: args.arguments.keyword,
+};
+
+/**
+ * @param {Quote} quote
+ */
+function printQuote(quote) {
+  console.log(`${quote.character.name}\n\t«${quote.sentence}»`);
+}
+
+ossApi.getQuotes(options).then((quotes) => {
+  quotes.forEach((quote) => {
+    printQuote(quote);
+  });
+});
